513890d52a02246801aee526ca9eddc3c51bc9af
yaoz <971210502@qq.com>
2024-01-09 20:25:26 +0800

    Update index.ts
    记录每次崩盘收益

--- src/index.ts
+++ src/index.ts
@@ -11,17 +11,27 @@ export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
   const logger = new Logger('stock')
+  var last_money = 0
+  
+  ctx.emit('iirose/stockGet', (data) => {
+    last_money = data.personalMoney
+  })
   /**
    * 2分钟执行一次，和花园股票刷新同步
    */
-  var last_price = -1
   ctx.on("iirose/before-getUserList", async (session) => {
     setTimeout(() => {
       ctx.emit('iirose/stockGet', (data) => {
-        if (data.unitPrice == 1) {
+        if (data.unitPrice == 1.00000) {
           result = 0
-          last_price = 1
-          logger.info(`股市已崩盘，当前金钱 ${data.personalMoney}`)
+          var msg = `本次崩盘收益 ${data.personalMoney-last_money} ，余额 ${data.personalMoney} 钞`
+          last_money = data.personalMoney // 记录上一次崩盘的钱
+          logger.info(msg)
+          session.send({
+            public:{
+              message:msg
+            }
+          })
         }
         else {
           var result = stock_calc1(data.unitPrice, data.totalStock, data.personalStock)
@@ -35,11 +45,10 @@ export function apply(ctx: Context) {
           logger.info(`价 ${data.unitPrice}，股 ${data.totalStock}，买 ${result}，余 ${data.personalMoney-data.unitPrice*result}`)
         } else {
           ctx.emit('iirose/stockSell', -result)
-          logger.info(`价 ${data.unitPrice}，股 ${data.totalStock}，卖 ${-result}，余 ${data.personalMoney+data.unitPrice*result}`)
+          logger.info(`价 ${data.unitPrice}，股 ${data.totalStock}，卖 ${-result}，余 ${data.personalMoney-data.unitPrice*result}`)
         }
-        last_price = data.unitPrice // 记录上一次价格
       })
-    }, 10000)
+    }, 100000)
   })
 
   /**
@@ -68,7 +77,7 @@ function stock_calc1(price, stock, personalStock) {
 
   if (price == 1) return result
   if (price < 0.1) return result
-  if (price < 0.4) {
+  if (price < 0.5) {
     // 决定持有0~400股
     if (price < 0.23) result += 60
     if (price < 0.3) result += 60
@@ -78,14 +87,14 @@ function stock_calc1(price, stock, personalStock) {
     if (result < personalStock && result > 0) result = 0
     else result = result - personalStock
   } else {
-    result = -personalStock * 0.25
-    if (price > 0.6) result -= personalStock * 0.0625
+    result = -personalStock * 0.125
     if (price > 0.7) result -= personalStock * 0.0625
     if (price > 0.8) result -= personalStock * 0.0625
-    if (price > 0.9) result -= personalStock * 0.125
-    if (price > 1.0) result -= personalStock * 0.125
-    if (price > 2.2) result -= personalStock * 0.0625
-    if (price > 2.8) result -= personalStock * 0.0625
+    if (price > 0.9) result -= personalStock * 0.0625
+    if (price > 1.0) result -= personalStock * 0.0625
+    if (price > 1.2) result -= personalStock * 0.125
+    if (price > 1.5) result -= personalStock * 0.125
+    if (price > 1.9) result -= personalStock * 0.125
     if (price > 3.5) return -personalStock
     if (-result > personalStock) result = -personalStock
   }
