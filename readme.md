ad512f193a1a44783180e1384646cbd428ebb455
yaoz <971210502@qq.com>
2024-01-10 12:40:27 +0800

    调参
    变成更无情的梭哈机器

--- readme.md
+++ readme.md
@@ -1,5 +1,17 @@
-# koishi-plugin-auto-stock
+# 自动炒股插件
 
-[![npm](https://img.shields.io/npm/v/koishi-plugin-auto-stock?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-auto-stock)
+## 部署
 
-aaaa
+1.  [[koishi]花园机器人搭建教程](https://forum.reifuu.icu/d/9-koishihua-yuan-ji-qi-ren-da-jian-jiao-cheng)
+2.  放置于`../external`目录下
+3.  `npm run`
+4.  在`插件配置->添加插件`添加本插件
+5.  以开发者模式重新启动机器人
+    `npm run dev`
+
+## 一些想更新的内容
+
+- 在插件配置里改变算法参数（什么时候买，买多少）
+- 另一种算法（买入后按照当前价格找机会卖出）
+- 另一种算法
+  以固定资金入场，跌20%买20%，涨20%卖20%
--- src/index.ts
+++ src/index.ts
@@ -23,15 +23,15 @@ export function apply(ctx: Context) {
     setTimeout(() => {
       ctx.emit('iirose/stockGet', (data) => {
         if (data.unitPrice == 1.00000) {
-          result = 0
+          result = 5
           var msg = `本次崩盘收益 ${data.personalMoney-last_money} ，余额 ${data.personalMoney} 钞`
           last_money = data.personalMoney // 记录上一次崩盘的钱
           logger.info(msg)
-          session.send({
-            public:{
-              message:msg
-            }
-          })
+          // session.send({
+          //   public:{
+          //     message:msg
+          //   }
+          // })
         }
         else {
           var result = stock_calc1(data.unitPrice, data.totalStock, data.personalStock)
@@ -42,10 +42,10 @@ export function apply(ctx: Context) {
         }
         if (result > 0) {
           ctx.emit('iirose/stockBuy', result)
-          logger.info(`价 ${data.unitPrice}，股 ${data.totalStock}，买 ${result}，余 ${data.personalMoney-data.unitPrice*result}`)
+          // logger.info(`价 ${data.unitPrice}，股 ${data.totalStock}，买 ${result}，余 ${data.personalMoney-data.unitPrice*result}`)
         } else {
           ctx.emit('iirose/stockSell', -result)
-          logger.info(`价 ${data.unitPrice}，股 ${data.totalStock}，卖 ${-result}，余 ${data.personalMoney-data.unitPrice*result}`)
+          // logger.info(`价 ${data.unitPrice}，股 ${data.totalStock}，卖 ${-result}，余 ${data.personalMoney-data.unitPrice*result}`)
         }
       })
     }, 100000)
@@ -57,7 +57,8 @@ export function apply(ctx: Context) {
   ctx.on('message', (session) => {
     if (session.content == `查股票`) {
       ctx.emit('iirose/stockGet', (data) => {
-        session.send(`当前股价 ${data.unitPrice}，总股 ${data.totalStock}，持股 ${data.personalStock}，余额 ${data.personalMoney}`)
+        var msg = `当前股价 ${data.unitPrice}，总股 ${data.totalStock}，持股 ${data.personalStock}，余额 ${data.personalMoney}`
+        session.send(msg)
       })
     }
   })
@@ -75,9 +76,9 @@ function stock_calc1(price, stock, personalStock) {
   var multi = 4000 / stock
   var result = 0
 
-  if (price == 1) return result
+  if (price == 1.00000) return result
   if (price < 0.1) return result
-  if (price < 0.5) {
+  if (price < 0.7) {
     // 决定持有0~400股
     if (price < 0.23) result += 60
     if (price < 0.3) result += 60
@@ -88,13 +89,13 @@ function stock_calc1(price, stock, personalStock) {
     else result = result - personalStock
   } else {
     result = -personalStock * 0.125
-    if (price > 0.7) result -= personalStock * 0.0625
-    if (price > 0.8) result -= personalStock * 0.0625
-    if (price > 0.9) result -= personalStock * 0.0625
-    if (price > 1.0) result -= personalStock * 0.0625
-    if (price > 1.2) result -= personalStock * 0.125
-    if (price > 1.5) result -= personalStock * 0.125
-    if (price > 1.9) result -= personalStock * 0.125
+    // if (price > 0.7) result -= personalStock * 0.0625
+    // if (price > 0.8) result -= personalStock * 0.0625
+    // if (price > 0.9) result -= personalStock * 0.0625
+    // if (price > 1.0) result -= personalStock * 0.0625
+    if (price > 1.2) result -= personalStock * 0.25
+    if (price > 1.5) result -= personalStock * 0.25
+    if (price > 1.9) result -= personalStock * 0.25
     if (price > 3.5) return -personalStock
     if (-result > personalStock) result = -personalStock
   }
